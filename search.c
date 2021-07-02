60f31b548d324cd91a374aeb53291d83d260ba2c
AkliYalaoui <akliyalaoui16@gmail.com>
2021-07-02 13:41:13 +0100

    search for files in all sub directories

--- search.c
+++ search.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <dirent.h>
 
+int is_a_directory(struct dirent *de);
+int is_it_our_file(struct dirent *de,char *file_name);
+void recursive_file_search(char *dir_name ,char *file_name);
 void search_file(char *dir_name,char *file_name);
 
 void main(int argc,char *argv[]){
@@ -18,11 +21,45 @@ void main(int argc,char *argv[]){
     if(argc == 2){
         search_file(".",file_name);
     }else if(argc == 3){
-         search_file(argv[argc-2],file_name);
+         search_file(argv[1],file_name);
     }else if(argc >= 4){
 
     }
 
+}
+
+int is_it_our_file(struct dirent *de,char *file_name){
+    return de->d_type != DT_DIR && strcmp(de->d_name,file_name) == 0 ? 0 : 1;
+}
+
+int is_a_directory(struct dirent *de){
+    return de->d_type == DT_DIR && strcmp(de->d_name,".") != 0 && strcmp(de->d_name,"..") != 0 ? 0:1;
+}
+void recursive_file_search(char *dir_name ,char *file_name){
+
+    DIR *dr = opendir(dir_name);
+    struct dirent *de;
+    char new_dir_name[1000];
+
+    if(dr == NULL) return;
+    while ((de = readdir(dr)) != NULL){
+
+        if(is_it_our_file(de,file_name) == 0){
+            if(strcmp(dir_name, "/") == 0)
+                printf("/%s\n",de->d_name); 
+            else
+                printf("%s/%s\n",dir_name,de->d_name); 
+        }   
+        else if(is_a_directory(de) == 0){
+
+            if(strcmp(dir_name, "/") == 0)
+                sprintf(new_dir_name, "%s%s", dir_name, de->d_name);
+            else
+                sprintf(new_dir_name, "%s/%s", dir_name, de->d_name);
+
+            recursive_file_search(new_dir_name,file_name);
+        }
+    }
 }
 
 void search_file(char *dir_name,char *file_name){
@@ -35,10 +72,6 @@ void search_file(char *dir_name,char *file_name){
          printf("Could not open %s directory",err_dir);
          return;
     }
-  
-    while ((de = readdir(dr)) != NULL)
-         if(strcmp(de->d_name, file_name) == 0)
-            printf("%s\n", de->d_name);
-
-    closedir(dr); 
+    closedir(dr);
+    recursive_file_search(dir_name,file_name); 
 }
